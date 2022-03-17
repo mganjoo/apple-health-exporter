d20b738a4acfc0f420683fea1c396231778cc030
Bruno Amaral <mail@brunoamaral.eu>
2022-03-17 20:43:10 +0000

    add description from readme

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
 
 setuptools.setup(
     name='Apple Health Exporter',
@@ -9,7 +7,14 @@ setuptools.setup(
     author='Milind Ganjoo',
     author_email='milind.ganjoo@gmail.com',
     description='Python script to export Apple Health dump file to a data frame for analysis.',
-    long_description=""
+    long_description="""
+# Apple Health Data Exporter
+
+This Python 3 script takes a dump of Apple Health data (the `export.zip` file
+generated through an Apple Health data export) and saves a subset of fields and
+records to a [feather](https://github.com/wesm/feather) file. This can then be
+read by both Python and R.
+    """,
     long_description_content_type="text/markdown",
     url='https://github.com/mganjoo/apple-health-exporter',
     project_urls = {
