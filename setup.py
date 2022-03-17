38cb261117c1d3024143acf15fe0a497bd28c63d
Bruno Amaral <mail@brunoamaral.eu>
2022-03-17 21:32:05 +0000

    working version, needs testing by someone more experienced

--- setup.py
+++ setup.py
@@ -2,27 +2,29 @@ import setuptools
 
 
 setuptools.setup(
-    name='Apple Health Exporter',
-    version='1.0.0',
-    author='Milind Ganjoo',
-    author_email='milind.ganjoo@gmail.com',
-    description='Python script to export Apple Health dump file to a data frame for analysis.',
-    long_description="""
+	name='apple health exporter',
+	version='1.0.0',
+	author='Milind Ganjoo',
+	author_email='milind.ganjoo@gmail.com',
+	description='Python script to export Apple Health dump file to a data frame for analysis.',
+	long_description="""
 # Apple Health Data Exporter
 
 This Python 3 script takes a dump of Apple Health data (the `export.zip` file
 generated through an Apple Health data export) and saves a subset of fields and
 records to a [feather](https://github.com/wesm/feather) file. This can then be
 read by both Python and R.
-    """,
-    long_description_content_type="text/markdown",
-    url='https://github.com/mganjoo/apple-health-exporter',
-    project_urls = {
-        "Bug Tracker": "https://github.com/mganjoo/apple-health-exporter/issues"
-    },
-    license='MIT',
-    packages=['apple-health-exporter'],
-    install_requires=['numpy','pandas','pyarrow','python-dateutil','pytz','six'],
+	""",
+	long_description_content_type="text/markdown",
+	url='https://github.com/mganjoo/apple-health-exporter',
+	project_urls = {
+		"Bug Tracker": "https://github.com/mganjoo/apple-health-exporter/issues"
+	},
+	license='MIT',
+	packages=[''],
+	package_dir={'':'applehealthexporter'},
+	scripts=['applehealthexporter/export.py'] ,
+	install_requires=['numpy','pandas','pyarrow','python-dateutil','pytz','six'],
 )
 
 
