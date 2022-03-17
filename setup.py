7250f4cf4aae0caef5b52fd83cbde1cd69f938e4
Bruno Amaral <mail@brunoamaral.eu>
2022-03-17 20:40:46 +0000

    no description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author='Milind Ganjoo',
     author_email='milind.ganjoo@gmail.com',
     description='Python script to export Apple Health dump file to a data frame for analysis.',
-    long_description=long_description,
+    long_description=""
     long_description_content_type="text/markdown",
     url='https://github.com/mganjoo/apple-health-exporter',
     project_urls = {
