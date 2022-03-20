a6bc8b45e4f11b92a16aa6cc71a0f31d2de3548a
Bruno Amaral <mail@brunoamaral.eu>
2022-03-20 20:52:15 +0000

    Set up as installable package (#9)
    Create a configuration for packaging the module and uploading to PyPI.
    
    Co-authored-by: Milind Ganjoo <milind.ganjoo@gmail.com>

---
+++ apple_health_exporter/__main__.py
@@ -0,0 +1,4 @@
+from .export import export
+
+if __name__ == "__main__":
+	export()
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(
+	name='apple-health-exporter',
+	version='1.0.0',
+	author='Milind Ganjoo',
+	author_email='milind.ganjoo@gmail.com',
+	description='Python script to export Apple Health dump file to a data frame for analysis.',
+	long_description="""
+# Apple Health Data Exporter
+
+This Python 3 script takes a dump of Apple Health data (the `export.zip` file
+generated through an Apple Health data export) and saves a subset of fields and
+records to a [feather](https://github.com/wesm/feather) file. This can then be
+read by both Python and R.
+	""",
+	long_description_content_type="text/markdown",
+	url='https://github.com/mganjoo/apple-health-exporter',
+	project_urls = {
+		"Bug Tracker": "https://github.com/mganjoo/apple-health-exporter/issues"
+	},
+	license='MIT',
+	packages=['apple_health_exporter'],
+	package_dir={'':'.'},
+	entry_points = {
+		'console_scripts': [
+			'apple-health-exporter=apple_health_exporter.export:export',
+		]
+	},
+	install_requires=['numpy','pandas','pyarrow','python-dateutil','pytz','six','lxml'],
+)
+
+
+
+
